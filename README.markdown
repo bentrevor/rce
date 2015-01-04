c19260ab7b98c50b1c95faac8d31688306e362a9
Ben Trevor <btrevor@8thlight.com>
2015-01-04 14:31:30 -0600

    start setting up database

--- README.markdown
+++ README.markdown
@@ -1,7 +1,8 @@
 
 ## About
 
-"You're late.  And this is the most important test of your life.  Does anyone know what 2 + 2 equals?"
+"You're late.  And this is the most important test of your life.  Does anyone
+know what 2 + 2 equals?"
 
 "5?"
 
@@ -14,3 +15,26 @@
 "Uh, 4."
 
 "And exactly what formula did _you_ use?"
+
+## Thoughts
+
+This is going to be a "currency exchange" simulation with a very faulty
+database.  I don't know how a real currency exchange works though, so it won't
+be a particularly accurate simulation.  I'm really not sure how this is going to
+turn out, but here's what I'm thinking now:
+
+- On each turn, players decide what trades to make, and they are all executed at
+once (at the end of the turn).
+
+- Players will control hedge funds.  There will be a few "banks" that you can
+always make fair trades with.  There will be exchange rates between each
+currency - these rates might fluctuate from turn to turn.
+
+- Hopefully, the fun part will be to try construct trades that cause favorable
+data inconsistency.
+
+- I'm really unsure about the objective of the game.  I don't think it's
+zero-sum, since there's technically no resource that is limited.  So the
+players could try to team up to "generate" the most money in the system.  Or
+they could work together to beat the banks (whatever that means).  Or they
+could just try to maximize their own hedge fund's accounts.
--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	"database/sql"
 
-	_ "github.com/lib/pg"
+	_ "github.com/lib/pq"
 )
 
 type Page struct {
@@ -30,7 +31,7 @@ func menuHandler(w http.ResponseWriter, r *http.Request) {
 	menuPage, err := readHTMLFile("menu")
 
 	if err != nil {
-		panic("there was an error reading the file menu.html")
+		log.Fatal("there was an error reading the file menu.html")
 	}
 
 	fmt.Fprintf(w, string(menuPage.Body))
@@ -41,11 +42,61 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	db, _ := sql.Open("postgres", "user=rcse_admin dbname=rcse_dev")
-	fmt.Println("starting server...\n")
+	fmt.Println("connecting to db...")
+	// TODO don't disable ssl...
+	db, err := sql.Open("postgres", "user=rcfe_admin dbname=rcfe_dev sslmode=disable")
+
+	if err != nil {
+		log.Fatal("failure connecting to database: ", err)
+	}
+
+	fmt.Println("seeding db...")
+	seedDB(db)
+
+	rows, e := db.Query("select name,dollars from hedge_funds;")
+	defer rows.Close()
+
+	if e != nil {
+		log.Fatal("failure querying the database: ", e)
+	}
+
+	for rows.Next() {
+		var dollars int
+		var name string
+		rows.Scan(&name, &dollars)
+		fmt.Printf("found the hedge fund %s with $%d\n\n", name, dollars)
+	}
+
+	fmt.Println("registering handlers...")
 
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/menu/", menuHandler)
 
+	fmt.Println("starting server...")
+
 	http.ListenAndServe(":8080", nil)
+}
+
+func seedDB(db *sql.DB) {
+	_, err := db.Query(`
+drop table if exists hedge_funds;
+create table hedge_funds (
+  id      serial,
+  name    varchar(50),
+  dollars integer
+);
+
+insert into hedge_funds (name, dollars) values ('asdf', 123);
+
+drop table if exists banks;
+create table banks (
+  id      serial,
+  name    varchar(50),
+  dollars integer
+);
+`)
+
+	if err != nil {
+		log.Fatal("error seeding the database: ", err)
+	}
 }
