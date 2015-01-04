109a92eb75c8aac4eee84ce52e3a8c64175a1cf8
Ben Trevor <btrevor@8thlight.com>
2015-01-04 14:49:48 -0600

    refactor, hedge_funds#index

--- main.go
+++ main.go
@@ -16,6 +16,9 @@ type Page struct {
 	Body  []byte
 }
 
+var db *sql.DB
+var err error
+
 func readHTMLFile(title string) (*Page, error) {
 	filename := title + ".html"
 	body, err := ioutil.ReadFile(filename)
@@ -41,18 +44,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "root")
 }
 
-func main() {
-	fmt.Println("connecting to db...")
-	// TODO don't disable ssl...
-	db, err := sql.Open("postgres", "user=rcfe_admin dbname=rcfe_dev sslmode=disable")
-
-	if err != nil {
-		log.Fatal("failure connecting to database: ", err)
-	}
-
-	fmt.Println("seeding db...")
-	seedDB(db)
-
+func hedgeFundsHandler(w http.ResponseWriter, r *http.Request) {
 	rows, e := db.Query("select name,dollars from hedge_funds;")
 	defer rows.Close()
 
@@ -64,20 +56,37 @@ func main() {
 		var dollars int
 		var name string
 		rows.Scan(&name, &dollars)
-		fmt.Printf("found the hedge fund %s with $%d\n\n", name, dollars)
+		fmt.Fprintf(w, fmt.Sprintf("hedge fund %s has $%d!\n\n", name, dollars))
 	}
+}
 
+func connectToDb() {
+	fmt.Println("connecting to db...")
+	// TODO don't disable ssl...
+	db, err = sql.Open("postgres", "user=rcfe_admin dbname=rcfe_dev sslmode=disable")
+
+	if err != nil {
+		log.Fatal("failure connecting to database: ", err)
+	}
+}
+
+func routeHandlers() {
 	fmt.Println("registering handlers...")
 
 	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/hedge_funds", hedgeFundsHandler)
 	http.HandleFunc("/menu/", menuHandler)
+}
 
+func startServer() {
 	fmt.Println("starting server...")
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func seedDB(db *sql.DB) {
+func seedDB() {
+	fmt.Println("seeding db...")
+
 	_, err := db.Query(`
 drop table if exists hedge_funds;
 create table hedge_funds (
@@ -86,7 +95,10 @@ create table hedge_funds (
   dollars integer
 );
 
-insert into hedge_funds (name, dollars) values ('asdf', 123);
+insert into hedge_funds (name, dollars) values ('hf1', 1234),
+                                               ('hf2', 5678),
+                                               ('hf3', 9090)
+;
 
 drop table if exists banks;
 create table banks (
@@ -94,9 +106,21 @@ create table banks (
   name    varchar(50),
   dollars integer
 );
+
+insert into banks (name, dollars) values ('b1', 11111),
+                                         ('b2', 22222),
+                                         ('b3', 33333)
+;
 `)
 
 	if err != nil {
 		log.Fatal("error seeding the database: ", err)
 	}
+}
+
+func main() {
+	connectToDb()
+	seedDB()
+	routeHandlers()
+	startServer()
 }
